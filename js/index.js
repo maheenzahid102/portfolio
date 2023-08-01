fe68e4770aaff38a0822c9312b8f16ac5ac77e74
maheenzahid102 <124422204+maheenzahid102@users.noreply.github.com>
2023-08-01 07:56:03 -0700

    change
    change

--- js/index.js
+++ js/index.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p , .about-content ',{ origin: 'right'});
 
 // ================== typed js ==================
 const typed = new Typed('.multiple-text', {
-    strings : ['Flutter Developer','Android Developer', 'UI/UX Designer','Sofware Engineer'],
+    strings : ['Programmer','Web Developer', 'Software Engineer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
